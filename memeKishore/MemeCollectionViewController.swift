b6d2a3fd9118f4d45f6c2ff8c25411f0588f40f5
kishorekris <kishorekris@vmware.com>
2020-05-07 12:25:03 +0530

    Added flow layout and its delegate

--- memeKishore/MemeCollectionViewController.swift
+++ memeKishore/MemeCollectionViewController.swift
@@ -9,13 +9,25 @@
 import Foundation
 import UIKit
 
-class MemeCollectionViewController : UICollectionViewController {
+class MemeCollectionViewController : UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    private let spacing:CGFloat = 16.0
     
+    @IBOutlet weak var flowLayout : UICollectionViewFlowLayout!
     
     var memes : [MemeModel]! {
         let object = UIApplication.shared.delegate
         let appdelegate = object as! AppDelegate
         return appdelegate.memes
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidLoad()
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        self.collectionView?.collectionViewLayout = layout
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,10 +47,23 @@ class MemeCollectionViewController : UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let detailController = self.storyboard?.instantiateViewController(identifier: "memedetailview") as! MemeDetailVC
-        detailController.memes = memes[indexPath.row]
+        detailController.memes = memes[indexPath.item]
         self.navigationController?.pushViewController(detailController, animated: true)
         
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfItemsPerRow:CGFloat = 4
+        let spacingBetweenCells:CGFloat = 16
+        
+        let totalSpacing = (2 * self.spacing) + ((numberOfItemsPerRow - 1) * spacingBetweenCells) //Amount of total spacing in a row
+        
+        if let collection = self.collectionView{
+            let width = (collection.bounds.width - totalSpacing)/numberOfItemsPerRow
+            return CGSize(width: width, height: width)
+        }else{
+            return CGSize(width: 0, height: 0)
+        }
+    }
 }
 
